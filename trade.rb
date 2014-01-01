72045a402195dc762d4fe7deba31f719e097b63d
jesseclay <jesseclay@gmail.com>
2013-12-31 16:21:41 -0800

    more commenting

--- trade.rb
+++ trade.rb
@@ -6,11 +6,13 @@ AMOUNT = "0.998"
 SLEEP_DURATION = 1
 ORDER_PLACED = false
 
-while true do 
-  sleep SLEEP_DURATION
+while true do # ping btce forever
+
+  sleep SLEEP_DURATION # added this to prevent nonce increment errors (pinging server too fast)
 
   order_list = Btce::TradeAPI.new_from_keyfile.order_list
   p order_list
+  # go through all current orders and see if this pair has a trade placed yet
   if order_list["success"] == 1
     for id in order_list["return"]
       if id[1]["pair"] == pair
@@ -27,7 +29,7 @@ while true do
     p Btce::TradeAPI.new_from_keyfile.trade buy_json
 
     # place sell order for PROFIT_MARGIN percentage above buy price
-    rate = ((ticker.buy * PROFIT_MARGIN) + ticker.buy).round(5).to_s
+    rate = ((ticker.buy * PROFIT_MARGIN) + ticker.buy).round(5).to_s #rounded for btc accuracy
     sell_json = {"pair" => PAIR, "type" => "sell", "rate" => rate, "amount" => AMOUNT}
     p Btce::TradeAPI.new_from_keyfile.trade sell_json
   end
